b7271dc1e4d9c216601d8f96df722cbaaaa8421d
Indrani Simhadri <simhadriindrani2410@gmail.com>
2025-06-02 15:47:16 +0530

    Initial Project Setup

---
+++ src/test/java/com/example/easydb/EasydbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
