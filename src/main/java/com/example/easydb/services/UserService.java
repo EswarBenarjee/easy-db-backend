3f6dcb8badee230aba8b8fcbe4c36f417b29d9ab
Eswar Benarjee <eswar.bane2002.india@gmail.com>
2025-07-07 02:40:44 +0530

    Adding Password Hashing

--- src/main/java/com/example/easydb/services/UserService.java
+++ src/main/java/com/example/easydb/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.easydb.services;
 
 import com.example.easydb.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.easydb.repositories.UserRepository;
 
@@ -10,10 +11,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User register(User user) {
         if(user == null) {
             return null;
         }
+
+        // Hash the password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
     }
